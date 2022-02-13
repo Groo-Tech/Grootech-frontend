e663b062d10c1cfc6105d7a66832ab2ed9d8f663
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-02-13 06:40:43 +0100

    updating UI

--- index.php
+++ index.php
@@ -4,12 +4,79 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/images/grootech_logo.png" type="image/x-icon">
     <link rel="stylesheet" href="css/home.css">
-    <title>Grootech | Agro-services made available</title>
+    <title>Grootech</title>
 </head>
 <body>
-    <div class="container">
+    <header class="header">
+        <div class="container">
+            <div class="header__wrapper">
+                <div class="header__logo">
+                    <a href="#" class="header__logo-link">
+                        <img src="./assets/images/grootech_logo.png" alt="grootech logo" class="header__logo-image">
+                        <p class="header__logo-text">
+                            Grootech
+                        </p>
+                    </a>
+                </div>
+                <nav class="navigation">
+                    <ul class="navigation__list">
+                        <li class="navigation__item">
+                            <a href="#" class="navigation__link" name="about us">
+                                About Us
+                            </a>
+                        </li>
+                        <li class="navigation__item">
+                            <a href="#" class="navigation__link" name="partners">
+                                Partners
+                            </a>
+                        </li>
+                        <li class="navigation__item">
+                            <a href="#" class="navigation__link" name="about us">
+                                Opportunities
+                            </a>
+                        </li>
+                        <li class="navigation__item">
+                            <a href="#" class="navigation__link" name="about us">
+                                Sign In
+                            </a>
+                        </li>
+                        <li class="navigation__item">
+                            <a href="#" class="btn btn--join" name="join grootech">
+                                Join Grootech
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <section class="section-intro">
+        <div class="intro">
+            <h1 class="intro__header">
+                Invest in the agricultural sector today
+            </h1>
+            <h2 class="intro__heading">
+                Search our business directory
+            </h2>
+            <form action="#" class="search">
+                <div class="search__wrapper">
+                    <div class="search__group">
+                        <div class="search__box">
+                            <div class="search__icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+                            </div>
+                            <input type="text" class="search__input">
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="overlay">&nbsp;</div>
+    </section>
+    <section class="section">
         &nbsp;
-    </div>
+    </section>
 </body>
 </html>
