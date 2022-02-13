74a16015fe6ee03973eb3660702c7ebda3850a7b
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-02-13 15:21:35 +0100

    updating UI

---
+++ search_result.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Grootech is an agro-based platform with the primary purpose of interconnecting all the players in the agricultural sector">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/images/grootech_logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/search_result.css">
+    <title>Grootech</title>
+</head>
+<body>
+    
+</body>
+</html>
