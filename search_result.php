1c279d927f38cb74cb80a4c2f720db9bcf03ebbc
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-02-16 06:34:56 +0100

    updating UI

--- search_result.php
+++ search_result.php
@@ -68,118 +68,154 @@
                 </p>
                 <ul class="results__list">
                     <li class="results__item">
-                        <a href="" class="results__link" name="Cattle Farm">
-                            <span class="results__image-box">
-                                <span class="results__button results__button--left">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
-                                </span>
-                                <span class="results__button results__button--right">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
-                                </span>
-                                <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
-                                <span class="results__controls">
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                </span>
+                        <a href="" class="results__link" name="Cattle Farm"></a>
+                        <span class="results__image-box">
+                            <a href="" class="results__link" name="Cattle Farm"></a>
+                            <button class="results__button results__button--left">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
+                            </button>
+                            <button class="results__button results__button--right">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                            </button>
+                            <img src="./assets/images/cities/grootech__city--2.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--3.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--4.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--5.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--6.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
+                            <span class="results__controls">
+                                <button class="results__dot results__dot--active">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
                             </span>
-                        </a>
+                        </span>
+                        <div class="results__details">
+                            <div class="results__favourite">
+                                <button class="icon-favourite">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                                </button>
+                            </div>
+                            <p class="results__paragraph">
+                                Entire unit in Lagos
+                            </p>
+                            <h1 class="results__heading">
+                                Agbalanjo Farms (ka sa ma du pe)
+                            </h1>
+                            <ul class="results__tags">
+                                <li class="results__tag">
+                                    Cattle Farming
+                                </li>
+                                <li class="results__tag">
+                                    Poultry Farming
+                                </li>
+                                <li class="results__tag">
+                                    Egg Production
+                                </li>
+                                <li class="results__tag">
+                                    Juice
+                                </li>
+                                <li class="results__tag">
+                                    Ranching
+                                </li>
+                                <li class="results__tag">
+                                    Milk
+                                </li>
+                                <li class="results__tag">
+                                    Hatchery
+                                </li>
+                            </ul>
+                        </div>
                     </li>
                     <li class="results__item">
-                        <a href="" class="results__link" name="Cattle Farm">
-                            <span class="results__image-box">
-                                <span class="results__button results__button--left">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
-                                </span>
-                                <span class="results__button results__button--right">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
-                                </span>
-                                <img src="./assets/images/cities/grootech__city--4.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--4.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--4.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--4.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--4.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--4.jpg" alt="Cattle Farm" class="results__image">
-                                <span class="results__controls">
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                </span>
+                        <a href="" class="results__link" name="Cattle Farm"></a>
+                        <span class="results__image-box">
+                            <a href="" class="results__link" name="Cattle Farm"></a>
+                            <button class="results__button results__button--left">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
+                            </button>
+                            <button class="results__button results__button--right">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                            </button>
+                            <img src="./assets/images/cities/grootech__city--5.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--6.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--7.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--8.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--4.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--2.jpg" alt="Cattle Farm" class="results__image">
+                            <span class="results__controls">
+                                <button class="results__dot results__dot--active">
+                                &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
                             </span>
-                        </a>
+                        </span>
                     </li>
                     <li class="results__item">
-                        <a href="" class="results__link" name="Cattle Farm">
-                            <span class="results__image-box">
-                                <span class="results__button results__button--left">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
-                                </span>
-                                <span class="results__button results__button--right">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
-                                </span>
-                                <img src="./assets/images/cities/grootech__city--8.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--8.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--8.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--8.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--8.jpg" alt="Cattle Farm" class="results__image">
-                                <img src="./assets/images/cities/grootech__city--8.jpg" alt="Cattle Farm" class="results__image">
-                                <span class="results__controls">
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                    <button class="results__dot">
-                                        &nbsp;
-                                    </button>
-                                </span>
+                        <a href="" class="results__link" name="Cattle Farm"></a>
+                        <span class="results__image-box">
+                            <a href="" class="results__link" name="Cattle Farm"></a>
+                            <button class="results__button results__button--left">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
+                            </button>
+                            <button class="results__button results__button--right">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                            </button>
+                            <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
+                            <img src="./assets/images/cities/grootech__city--1.jpg" alt="Cattle Farm" class="results__image">
+                            <span class="results__controls">
+                                <button class="results__dot results__dot--active">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
+                                <button class="results__dot">
+                                    &nbsp;
+                                </button>
                             </span>
-                        </a>
+                        </span>
                     </li>
                 </ul>
             </div>
