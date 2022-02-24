82aa6fb529ef47f9ebde10171ddfc286cadabba6
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-02-24 03:45:15 +0100

    updating UI

--- dashboard.php
+++ dashboard.php
@@ -20,6 +20,38 @@
                     </p>
                 </a>
             </div>
+            <ul class="dashboard__list">
+                <li class="dashboard__item">
+                    <a href="#" class="dashboard__link">
+                        <span class="">
+                            Wallet
+                        </span>
+                        <span class="icon-link">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-credit-card"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg>
+                        </span>
+                    </a>
+                </li>
+                <li class="dashboard__item">
+                    <a href="#" class="dashboard__link">
+                        <span class="">
+                            Messages
+                        </span>
+                        <span class="icon-link">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-circle"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
+                        </span>
+                    </a>
+                </li>
+                <li class="dashboard__item">
+                    <a href="#" class="dashboard__link">
+                        <span class="">
+                            Proposal
+                        </span>
+                        <span class="icon-link">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-credit-card"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg>
+                        </span>
+                    </a>
+                </li>
+            </ul>
         </div>
         <div class="dashboard__view">
             &nbsp;
