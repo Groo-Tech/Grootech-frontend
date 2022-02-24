2549b8649b01cb065d7d975c07ba0b6549aab292
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-02-24 04:12:06 +0100

    updating UI

--- dashboard.php
+++ dashboard.php
@@ -13,7 +13,7 @@
     <div class="dashboard">
         <div class="dashboard__sidebar">
             <div class="logo">
-                <a href="#" class="logo__link">
+                <a href="" class="logo__link">
                     <img src="./assets/images/grootech_logo.png" alt="grootech logo" class="logo__image">
                     <p class="logo__text">
                         Grootech
@@ -22,7 +22,7 @@
             </div>
             <ul class="dashboard__list">
                 <li class="dashboard__item">
-                    <a href="#" class="dashboard__link">
+                    <a href="" class="dashboard__link">
                         <span class="">
                             Wallet
                         </span>
@@ -32,7 +32,7 @@
                     </a>
                 </li>
                 <li class="dashboard__item">
-                    <a href="#" class="dashboard__link">
+                    <a href="" class="dashboard__link">
                         <span class="">
                             Messages
                         </span>
@@ -42,15 +42,58 @@
                     </a>
                 </li>
                 <li class="dashboard__item">
-                    <a href="#" class="dashboard__link">
+                    <a href="" class="dashboard__link">
                         <span class="">
                             Proposal
                         </span>
                         <span class="icon-link">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-credit-card"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-briefcase"><rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect><path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path></svg>
                         </span>
                     </a>
                 </li>
+                <li class="dashboard__item">
+                    <a href="" class="dashboard__link">
+                        <span class="">
+                            Timeline
+                        </span>
+                        <span class="icon-link">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline></svg>
+                        </span>
+                    </a>
+                </li>
+                <div class="cta">
+                    <div class="cta__image-box">
+                        <img src="./assets/images/grootech_logo.png" alt="" class="cta__image">
+                    </div>
+                    <p class="cta__paragraph">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    </p>
+                    <a href="" class="btn btn--verify">
+                        Verify Account
+                    </a>
+                </div>
+                <div class="dashboard__footer">
+                    <li class="dashboard__item">
+                        <a href="" class="dashboard__link">
+                            <span class="icon-link">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
+                            </span>
+                            <span class="">
+                                Settings
+                            </span>
+                        </a>
+                    </li>
+                    <li class="dashboard__item">
+                        <a href="" class="dashboard__link">
+                            <span class="icon-link">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
+                            </span>
+                            <span class="">
+                                Logout
+                            </span>
+                        </a>
+                    </li>
+                </div>
             </ul>
         </div>
         <div class="dashboard__view">
