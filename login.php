42e0b9bb96179107914d4be948fa79844eb199dd
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-03-21 11:55:17 +0100

    updating UI

--- login.php
+++ login.php
@@ -140,7 +140,48 @@
     </section>
     <!-- mobile navigation -->
     <footer class="footer">
-        &nbsp;
+        <ul class="footer__list">
+            <li class="footer__item">
+                <a href="" class="footer__link" name="Home">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                    </span>
+                    <span class="text">
+                        Home
+                    </span>
+                </a>
+            </li>
+            <li class="footer__item">
+                <a href="" class="footer__link" name="Explore">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-compass"><circle cx="12" cy="12" r="10"></circle><polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon></svg>
+                    </span>
+                    <span class="text">
+                        Explore
+                    </span>
+                </a>
+            </li>
+            <li class="footer__item">
+                <a href="" class="footer__link" name="sign-up">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                    </span>
+                    <span class="text">
+                        Sign Up
+                    </span>
+                </a>
+            </li>
+            <li class="footer__item">
+                <button class="footer__link" name="Dark-mode">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                    </span>
+                    <span class="text">
+                        Dark Mode
+                    </span>
+                </button>
+            </li>
+        </ul>
     </footer>
 </body>
 </html>
