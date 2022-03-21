d145ae9c5dbda6641de2363775ae01585a3a28de
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-03-21 15:56:54 +0100

    updating UI

---
+++ finalize-login.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Grootech is an agro-based platform with the primary purpose of interconnecting all the players in the agricultural sector">
+    <link rel="shortcut icon" href="./assets/illustrations/grootech__logo.svg" type="image/x-icon">
+    <link rel="stylesheet" href="css/finalize-login.css">
+    <title>Grootech | Signing Up</title>
+</head>
+<body>
+    <section class="section-header">
+        <header class="header header--2 header--fixed ">
+            <div class="container u-w-full">
+                <div class="header__wrapper">
+                    <div class="header__logo">
+                        <a href="" class="header__logo-link">
+                            <img src="./assets/illustrations/grootech__logo.svg" alt="grootech logo" class="header__logo-image">
+                            <p class="header__logo-text">
+                                Grootech
+                            </p>
+                        </a>
+                    </div>
+                    <div class="header__profile">
+                        <button class="header__icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
+                        </button>
+                        <div class="header__image-box">
+                            <span class="header__image header__image--blank">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                            </span>
+                            <!-- <img src="./assets/images/avatars/avatar--1.png" alt="" class="header__image"> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </section>
+    <section class="section-login u-mb-sm">
+        <div class="container">
+            <div class="login">
+                <form action="" class="form">
+                    <div class="form__header">
+                        <p class="form__paragraph">
+                            Finish signing up
+                        </p>
+                    </div>
+                    <div class="form__body">
+                        <div class="form__group u-mt-sm form__group--no-margin-bottom form__group--border-top form__group--border-top-radius form__group--border-round-top form__group--border-bottom">
+                            <label for="first-name" class="form__label">
+                                First name
+                            </label>
+                            <input type="text" class="form__input" id="first-name" placeholder="John" name="first-name">
+                        </div>
+                        <div class="form__group form__group--no-margin-bottom form__group--border-round-bottom form__group--border-bottom form__group--border-bottom-radius">
+                            <label for="last-name" class="form__label">Last name</label>
+                            <input type="text" class="form__input" placeholder="Doe" name="last-name">
+                        </div>
+                        <p class="form__text">
+                            Make sure it matches the name on your government ID.
+                        </p>
+                        <div class="form__box">
+                            <label for="birthday" class="form__label--lg">
+                                <span class="text">
+                                    Birthday
+                                </span>
+                                <span class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-help-circle"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
+                                </span>
+                            </label>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    <!-- mobile navigation -->
+    <footer class="footer">
+        <ul class="footer__list">
+            <li class="footer__item">
+                <a href="" class="footer__link" name="Home">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                    </span>
+                    <span class="text">
+                        Home
+                    </span>
+                </a>
+            </li>
+            <li class="footer__item">
+                <a href="" class="footer__link" name="Explore">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-compass"><circle cx="12" cy="12" r="10"></circle><polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon></svg>
+                    </span>
+                    <span class="text">
+                        Explore
+                    </span>
+                </a>
+            </li>
+            <li class="footer__item">
+                <a href="" class="footer__link" name="sign-up">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                    </span>
+                    <span class="text">
+                        Sign Up
+                    </span>
+                </a>
+            </li>
+            <li class="footer__item">
+                <button class="footer__link" name="Dark-mode">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                    </span>
+                    <span class="text">
+                        Dark Mode
+                    </span>
+                </button>
+            </li>
+        </ul>
+    </footer>
+</body>
+</html>
