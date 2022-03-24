ff1d91425351eff61d2e4af522e2906540658175
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-03-24 13:27:47 +0100

    first commit

--- dashboard.php
+++ dashboard.php
@@ -81,6 +81,11 @@
                             </div>
                         </div>
                     </div>
+                    <button class="header__menu">
+                        <span class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+                        </span>
+                    </button>
                 </div>
             </div>
         </header>
@@ -169,6 +174,12 @@
                     <h1 class="account__heading">
                         Connections
                     </h1>
+                    <!-- desktop -->
+                    <p class="account__paragraph account__paragraph--desktop">
+                        <a href="" class="" name="All Connections">
+                            All Connections (0)
+                        </a>
+                    </p>
                 </div>
                 <div class="account__wrapper">
                     <ul class="account__tabs">
@@ -248,6 +259,12 @@
                         </div>
                     </div>
                 </div>
+                <!-- mobile -->
+                <p class="account__paragraph account__paragraph--mobile">
+                    <a href="" class="" name="All Connections">
+                        All Connections (0)
+                    </a>
+                </p>
             </div>
         </div>
     </section>
@@ -525,7 +542,23 @@
                         </span>
                     </div>
                     <div class="social__media">
-                        &nbsp;
+                        <ul class="social__list">
+                            <li class="social__item">
+                                <a href="" class="social__link" name="facebook">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
+                                </a>
+                            </li>
+                            <li class="social__item">
+                                <a href="" class="social__link" name="twitter">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
+                                </a>
+                            </li>
+                            <li class="social__item">
+                                <a href="" class="social__link" name="instagram">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
