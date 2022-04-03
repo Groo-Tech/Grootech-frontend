b3bc59d167530486ab78218f248e98ef02110453
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-04-03 15:55:20 +0100

    updating UI

---
+++ overview.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="color-scheme" content="dark light">
+    <meta name="description" content="Grootech is an agro-based platform with the primary purpose of interconnecting all the players in the agricultural sector">
+    <link rel="shortcut icon" href="./assets/illustrations/grootech__logo.svg" type="image/x-icon">
+    <link rel="stylesheet" href="css/overview.css">
+    <title>Grootech | Overview</title>
+</head>
+<body>
+    <section class="section-header">
+        <header class="header header--1 u-flex u-align-center">
+            <div class="container u-w-full">
+                <div class="header__wrapper">
+                    <div class="header__logo">
+                        <a href="" class="header__logo-link">
+                            <svg class="header__logo-image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 178.43 178.43"><defs><style>.cls-1{fill:var(--logo__brown--1);}.cls-2{fill:var(--logo__brown--2);}</style></defs><title>Asset 1</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M57.17,89.22H0A89.21,89.21,0,0,1,143.06,18.09l-41.4,41.59A32,32,0,0,0,57.17,89.22Z"/><path class="cls-2" d="M178.43,89.22v89.21H121.26V172.5A89.32,89.32,0,0,1,35.1,160.14L76.44,118.6a32,32,0,0,0,44.82-29.39Z"/></g></g></svg>
+                            <p class="header__logo-text">
+                                Grootech
+                            </p>
+                        </a>
+                    </div>
+                    <div class="options">
+                        <button class="options__text">
+                            <span>
+                                Start your search
+                            </span>
+                        </button>
+                        <button class="options__rating">
+                            <div class="options__search">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--grootech__white--fixed)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+                            </div>
+                        </button>
+                    </div>
+                    <div class="header__profile">
+                        <button class="header__icon btn-toggle">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--grootech__black)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>
+                        </button>
+                        <div class="header__image-box">
+                            <img src="./assets/images/avatars/avatar--1.png" alt="" class="header__image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </section>
+    <section class="section-overview">
+        <div class="container">
+            <div class="overview">
+                <h1 class="overview__heading">
+                    🔥 Agbalanjo farms 🔥💦
+                </h1>
+                <div class="overview__details overview__details--reviewed">
+                    <p class="overview__rating">
+                        <span class="icon icon--star">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
+                        </span>
+                        <span class="text">
+                            5.0
+                        </span>
+                    </p>
+                    <p class="overview__reviews">
+                        <span class="dot"></span>
+                        <span class="text">
+                            10 reviews
+                        </span>
+                    </p>
+                    <p class="overview__location">
+                        <span class="dot"></span>
+                        <span class="icon icon--map">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
+                        </span>
+                        <span class="text">
+                            Lagos, Nigeria
+                        </span>
+                    </p>
+
+                    <div class="overview__actions">
+                        <button class="overview__share">
+                            <span class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share-2"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line></svg>
+                            </span>
+                            <span class="text">
+                                Share
+                            </span>
+                        </button>
+                        <button class="overview__save">
+                            <span class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                            </span>
+                            <span class="text">
+                                Save
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div class="overview__gallery">
+                    <div class="overview__image-box overview__image-box--lg">
+                        <img src="./assets/images/businesses/grootech__image--1.jpg" alt="" class="overview__image overview__image--curved-left overview__image--lg">
+                    </div>
+                    <div class="overview__image-box overview__image-box--sm">
+                        <img src="./assets/images/businesses/grootech__image--3.jpg" alt="" class="overview__image overview__image--sm">
+                        <img src="./assets/images/businesses/grootech__image--4.jpg" alt="" class="overview__image overview__image--sm overview__image--curved-top-right">
+                        <img src="./assets/images/businesses/grootech__image--5.jpg" alt="" class="overview__image overview__image--sm ">
+                        <img src="./assets/images/businesses/grootech__image--8.jpg" alt="" class="overview__image overview__image--sm overview__image--curved-bottom-right">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section-details">
+        <div class="container">
+            <div class="details">
+                <div class="details__view">
+                    <div class="details__header">
+                        <div class="details__info">
+                            <h1 class="details__heading">
+                                Entire farm hosted by Sam
+                            </h1>
+                            <ul class="details__tags">
+                                <li class="details__tag">
+                                    <span class="text">
+                                        Cattle ranch
+                                    </span>
+                                </li>
+                                <li class="details__tag">
+                                    <span class="dot"></span>
+                                    <span class="text">
+                                        Poultry
+                                    </span>
+                                </li>
+                                <li class="details__tag">
+                                    <span class="dot"></span>
+                                    <span class="text">
+                                        Forestry
+                                    </span>
+                                </li>
+                                <li class="details__tag">
+                                    <!-- <span class="dot"></span> -->
+                                    <span class="text">
+                                        +5
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                        <button class="details__image-box">
+                            <img src="./assets/images/avatars/avatar--7.jpg" alt="" class="details__image">
+                        </button>
+                    </div>
+                    <ul class="details__features">
+                        <li class="details__feature">
+                            <span class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
+                            </span>
+                            <div class="details__info">
+                                <h3 class="details__title">
+                                    Lorem, ipsum dolor.
+                                </h3>
+                                <span class="text">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium rem est quibusdam.
+                                </span>
+                            </div>
+                        </li>
+                        <li class="details__feature">
+                            <span class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
+                            </span>
+                            <div class="details__info">
+                                <h3 class="details__title">
+                                    Lorem, ipsum dolor.
+                                </h3>
+                                <span class="text">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium rem est quibusdam.
+                                </span>
+                            </div>
+                        </li>
+                        <li class="details__feature">
+                            <span class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
+                            </span>
+                            <div class="details__info">
+                                <h3 class="details__title">
+                                    Lorem, ipsum dolor.
+                                </h3>
+                                <span class="text">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium rem est quibusdam.
+                                </span>
+                            </div>
+                        </li>
+                        <li class="details__feature">
+                            <span class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
+                            </span>
+                            <div class="details__info">
+                                <h3 class="details__title">
+                                    Lorem, ipsum dolor.
+                                </h3>
+                                <span class="text">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium rem est quibusdam.
+                                </span>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="details__tabs">
+                        <button class="details__tab details__tab--active">
+                            Overview
+                        </button>
+                        <button class="details__tab">
+                            Proposals
+                        </button>
+                        <button class="details__tab">
+                            Timeline
+                        </button>
+                    </div>
+                </div>
+                <div class="details__sidebar">
+                    &nbsp;
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="./Js/theme.js"></script>
+</body>
+</html>
