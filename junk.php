56cecc73fa53223909a8d663fb98622109f031ce
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-04-06 21:54:46 +0100

    updating UI

---
+++ junk.php
@@ -0,0 +1,17 @@
+<div class="container-small">
+                            <div class="tab-link">
+                                <ul class="tabs">
+                                    <li data-tab-target="#design" class="tab active">Design</li>
+                                    <li data-tab-target="#custom" class="tab">Custom</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="container-small tab-content-box">
+                            <div class="tab-content">
+                                <!-- Content -->
+                                <div id="design" data-tab-content class="active">
+</div>
+<div id="custom" data-tab-content>
+</div>
+</div>
+</div>
