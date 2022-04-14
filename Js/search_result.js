04db2e3aa7bbf029cc693b665bbeb721533767a8
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-04-14 22:09:46 +0100

    updating UI

--- Js/search_result.js
+++ Js/search_result.js
@@ -34,3 +34,38 @@ mobileMapButton.addEventListener("click", function (e) {
   mobileView.classList.remove("max");
   mobileViewBtn.style.display = "block";
 });
+
+const mobileSearchInput = document.querySelector(".modal__input");
+const clearMobileSearchInputBtn = document.querySelector(
+  ".modal__header .clear"
+);
+
+mobileSearchInput.addEventListener("keyup", function (e) {
+  if (mobileSearchInput.value.trim().length >= 0) {
+    clearMobileSearchInputBtn.style.display = "flex";
+  }
+
+  if (mobileSearchInput.value.trim() <= 0) {
+    clearMobileSearchInputBtn.style.display = "none";
+  }
+});
+
+clearMobileSearchInputBtn.addEventListener("click", function (e) {
+  mobileSearchInput.value = "";
+  mobileSearchInput.focus();
+  clearMobileSearchInputBtn.style.display = "none";
+});
+
+const mobileEditButton = document.querySelector(".edit__button");
+const mobileModalSearch = document.querySelector(".modal-search");
+const mobileModalExitBtn = document.querySelector(".modal__header .icon");
+
+mobileEditButton.addEventListener("click", function (e) {
+  mobileModalSearch.classList.remove("modal-search--hidden");
+  mobileSearchInput.focus();
+});
+
+mobileModalExitBtn.addEventListener("click", function (e) {
+  mobileSearchInput.blur();
+  mobileModalSearch.classList.add("modal-search--hidden");
+});
--- search_result.php
+++ search_result.php
@@ -11,12 +11,15 @@
     <title>Grootech</title>
 </head>
 <body>
-    <div class="modal-search">
+    <div class="modal-search modal-search--hidden">
         <div class="modal__header">
             <button class="icon">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
             </button>
             <input type="text" class="modal__input" placeholder="What are you looking for?" name="tag">
+            <button class="clear">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+            </button>
         </div>
         <div class="modal__body">
             <p class="modal__paragraph">
